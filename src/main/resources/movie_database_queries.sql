488cec1f63adb841323b8b755d2e40a8646a0c33
Mohit Saini <mohit.saini09868@gmail.com>
2022-09-15 23:05:58 +0530

    Get all the movies from the Movie table. movie_database_queries.sql

--- src/main/resources/movie_database_queries.sql
+++ src/main/resources/movie_database_queries.sql
@@ -1,6 +1,5 @@
 -- create a database called `movies`
   create database if not exists  `movies`;
-
 -- use the `movies` database
    use `movies`;
 -- create a table called `movie` with the following columns:
@@ -16,7 +15,6 @@ create table if not exists `movie`(
 `Revenue_In_Millions` int not null,
 `Rating_Average` double not null
 );
-
 -- insert the following data into the `movie` table:
 -- avatar, 237, 10-12-2009, 278, 7.2
 insert into `movies`.`movie`(`Title`,`Budget_In_Millions`,`Release_Date`,`Revenue_In_Millions`,`Rating_Average`) values ('Avatar', '237', 10/12/2009, '278', '7.2');
@@ -40,21 +38,31 @@ insert into `movies`.`movie` values ('Captain America: Civil War', 250,2016/04/2
 insert into `movies`.`movie` values ('Transformers: Dark Of The Moon', 195,2011/06/28, 112, 6.1);
 
 -- Get all the movies from the Movie table.
+select `title` from `movies`.`movie`;
 
 -- Fetch the movie with the name Jurassic World and a rating of 6.5.
+select `title`,`rating_average` from `movies`.`movie` where (`Title`= 'Jurassic World' && `Rating_Average` = 6.5) ;
 
 -- Retrieve all the movies that have a rating of 7 and above.
+select `title`from `movies`.`movie` where (`Rating_Average`>=7.0) ;
 
 -- Fetch all the movies that have the title Avengers.
+select `title`from `movies`.`movie` where (`Title`like '%Avengers%');
 
 -- Retrieve the title, and rating of all the movies that have a rating between 7 and 8.
+select `title`,`rating_average`from `movies`.`movie` where (`Rating_Average` between 7.0 and 8.0);
 
 -- Get the title of all the movies that have a budget of $250 million and more.
+select `title`from `movies`.`movie` where (`Budget_In_Millions`>= 250);
 
 -- Retrieve the title, and rating of all movies that had earned a revenue of $150 to $200 million.
+select `title`,`rating_average`from `movies`.`movie` where (`Revenue_In_Millions` between 150 and 200);
 
 -- Fetch the title and release date of all movies beginning with 'T'.
+select `title`,`release_date`from `movies`.`movie` where (`Title` like 'T%');
 
 --Retrieve the total revenue of all the movies together.
+select sum(`Revenue_In_Millions`)from `movies`.`movie`;
 
 --Get the title, revenue of the movie that has earned the least revenue.
+select`title`,`revenue_in_millions`from `movies`.`movie` where (`Revenue_In_Millions`= (select min(`Revenue_In_Millions`)from `movies`.`movie`));
